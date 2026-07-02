7c98fee80cfe1421e5b70b983f787dc26dda39ea
mrbasel7 <mrbaselagency@gmail.com>
2026-07-03 02:28:05 +0300

    Implement CORS support in API
    Added CORS middleware to allow cross-origin requests.

--- bot.js
+++ bot.js
@@ -487,6 +487,14 @@ bot.onText(/\/status/, (msg) => {
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 app.post("/check-code", (req, res) => {
   const { code } = req.body;
   if (!code) return res.json({ valid: false, message: "No code provided" });
